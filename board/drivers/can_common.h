2e4efd6cda01a903963993809157c9c1d199a920
MoreTore <ryleymccarroll22@gmail.com>
2025-05-08 04:23:22 +0200

    Mazda 0.9.8

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -207,6 +207,10 @@ void ignition_can_hook(CANPacket_t *to_push) {
       ignition_can = (GET_BYTE(to_push, 0) >> 5) == 0x6U;
       ignition_can_cnt = 0U;
     }
+    if ((addr == 0x274) && (len == 8)) {
+      ignition_can = (GET_BYTE(to_push, 5) & 0x4U) != 0U;
+      ignition_can_cnt = 0U;
+    }
 
   }
 }
